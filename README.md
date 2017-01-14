6990a596233557d86bfbd17c1381db58e7483f05
Gomah <hi@gomah.fr>
2017-01-15 01:54:33 +1100

    Init files

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # vue-places
+
+Places component is based on [places.js](https://github.com/algolia/places) for Vue 2.x.
+
+---------------
+
+## Installation
+
+```bash
+# yarn
+yarn add vue-places
+
+# npm
+$ npm install vue-places --save
+```
+
+## Example
+
+```vue
+<template>
+  <places placeholder="Where are we going ?" :options="{ countries: 'US' }"></places>
+</template>
+
+<script>
+import Places from 'vue-places'
+
+export default {
+  components: {
+    Places
+  }
+}
+</script>
+```
+
+---------------
+
+[Algolia Places Documentation](https://community.algolia.com/places/documentation.html#options)
---
+++ src/Places.vue
@@ -0,0 +1,40 @@
+<template>
+  <input type="search" v-bind:value="value" @input="updateValue($event.target.value)" />
+</template>
+<script>
+import places from 'places.js';
+
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    options: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+
+  data () {
+    return {
+      placesAutocomplete: null,
+    }
+  },
+
+  methods: {
+    updateValue(value) {
+      this.$emit('input', value);
+    },
+  },
+
+  mounted () {
+    this.options.container = this.options.container || this.$el;
+    this.placesAutocomplete = places(this.options);
+  },
+
+  beforeDestroy () {
+    this.placesAutocomplete.destroy();
+  }
+}
+</script>
